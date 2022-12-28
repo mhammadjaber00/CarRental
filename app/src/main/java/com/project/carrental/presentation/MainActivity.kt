78fa3c0e878561767072e7554f9e7086c277fc01
Mohammad Jaber <mhammadjaber00@outlook.com>
2022-12-29 00:15:45 +0200

    Initial Commit

---
+++ app/src/main/java/com/project/carrental/presentation/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.project.carrental.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.project.carrental.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+    }
+}
