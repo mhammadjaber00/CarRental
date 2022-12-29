871dceaf35c08baa736973aad373f8b4eeecbd7e
mohamad.jaber <mohamad.jaber@montymobile.com>
2022-12-29 19:03:55 +0200

    HELP

--- app/src/main/java/com/project/carrental/presentation/MainActivity.kt
+++ app/src/main/java/com/project/carrental/presentation/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.project.carrental.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.project.carrental.R
+import com.project.carrental.databinding.ActivityMainBinding
+import com.project.carrental.presentation.customer.CustomerMainActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCustomer.setOnClickListener {
+            startActivity(Intent(this, CustomerMainActivity::class.java))
+        }
     }
 }
