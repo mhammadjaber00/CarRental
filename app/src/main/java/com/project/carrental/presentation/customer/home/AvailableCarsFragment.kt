60c3c08ad0ddbfd24237b82163317d5baaba74b4
mohamad.jaber <mohamad.jaber@montymobile.com>
2022-12-30 14:47:36 +0200

    .

--- app/src/main/java/com/project/carrental/presentation/customer/home/AvailableCarsFragment.kt
+++ app/src/main/java/com/project/carrental/presentation/customer/home/AvailableCarsFragment.kt
@@ -4,20 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.project.carrental.databinding.FragmentHomeBinding
+import com.project.carrental.databinding.FragmentAvailableCarsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
+class AvailableCarsFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentAvailableCarsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val homeViewModel: HomeViewModel by viewModels()
+    private val availableCarsViewModel: AvailableCarsViewModel by viewModels()
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -26,14 +25,9 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        _binding = FragmentAvailableCarsBinding.inflate(inflater, container, false)
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/project/carrental/presentation/customer/home/AvailableCarsViewModel.kt
@@ -0,0 +1,10 @@
+package com.project.carrental.presentation.customer.home
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AvailableCarsViewModel @Inject constructor() : ViewModel() {
+
+}
