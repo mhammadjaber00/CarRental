5181a582a1b0529bb718f070940b21babd326a7f
Shatila <SaidShatila@outlook.com>
2023-01-07 16:42:52 +0200

    latest changes

--- app/src/main/java/com/project/carrental/presentation/LoginActivity.kt
+++ app/src/main/java/com/project/carrental/presentation/LoginActivity.kt
@@ -3,27 +3,40 @@ package com.project.carrental.presentation
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.project.carrental.R
 import com.project.carrental.databinding.ActivityLoginBinding
 import com.project.carrental.presentation.admin.AdminMainActivity
 import com.project.carrental.presentation.customer.CustomerMainActivity
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
-    private val isAdmin by lazy {
-        intent.getBooleanExtra("isAdmin", true)
-    }
+    private var isAdmin = true
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState : Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnSignIn.setOnClickListener {
-            if (isAdmin) {
-                startActivity(Intent(this, AdminMainActivity::class.java))
-                finishAffinity()
-            } else {
-                startActivity(Intent(this, CustomerMainActivity::class.java))
-                finishAffinity()
+        with(binding) {
+            rgUserSelection.setOnCheckedChangeListener { _, checkedId ->
+                when (checkedId) {
+                    R.id.rb_admin -> {
+                        isAdmin = true
+                    }
+                    R.id.rb_customer -> {
+                        isAdmin = false
+                    }
+                }
+            }
+            btnSignIn.setOnClickListener {
+                if (isAdmin) {
+                    startActivity(Intent(this@LoginActivity, AdminMainActivity::class.java))
+                    finishAffinity()
+                } else {
+                    startActivity(Intent(this@LoginActivity, CustomerMainActivity::class.java))
+                    finishAffinity()
+                }
             }
         }
     }
